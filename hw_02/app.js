b5f03ae03e00fb6a46e5b6a36ad130dea35edeea
sergkapustnyk <serg.mrkrabs@gmail.com>
2019-12-15 21:22:35 +0200

    First commit (Task 01-02_hw02)

---
+++ hw_02/app.js
@@ -0,0 +1,58 @@
+console.log(`HW02_Task01`);
+{
+    let a = prompt(`enter number of a =`, ``);
+    b = prompt(`enter number of b =`, ``);
+    if (a > b)
+        console.log(x = a + b / 2 * 4);
+    else if (a === b)
+        console.log(x = 400);
+    else if (a < b)
+        console.log(x = a - b + 2 /b * 4)
+}
+console.log (`HW02_Task02.1`);
+
+{
+    let x = prompt(`enter a coordinate on X-axis`, ``);
+    let y = prompt(`enter a coordinate on Y-axis`, ``);
+    if ((x >= -4 && x <= 4) && (y >= 0 && y <= 4))
+    alert(`Match`);
+    else
+    alert(`Fail`)
+}
+console.log (`HW02_Task02.2`);
+{
+    let x = prompt(`enter a coordinate on X-axis`, ``);
+    let y = prompt(`enter a coordinate on Y-axis`, ``);
+    if ((y <= 0 && y <= x && y === x && y >= -1) || (y >= 0 && y <= 1 && y === x && y >= x))
+        alert(`Match`);
+    else
+        alert(`Fail`)
+    }
+    console.log (`HW02_Task02.3`);
+{
+    let x = prompt(`enter a coordinate on X-axis`, ``);
+    let y = prompt(`enter a coordinate on Y-axis`, ``);
+    if (x * x + y * y <= 1)
+        alert(`Match`);
+    else if (y >= -x - 2 && y <= 0 && x <= 0)
+        alert(`Match`);
+    else
+        alert(`Fail`)
+}
+console.log(`HW02_Task03`);
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
